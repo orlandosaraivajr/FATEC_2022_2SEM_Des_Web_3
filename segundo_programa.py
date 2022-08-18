a9fa87618661c1b2b3cc5938288fb111e9ba8102
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2022-08-17 22:17:42 -0300

    Primeira aula

---
+++ segundo_programa.py
@@ -0,0 +1,7 @@
+nome = input('Digite seu nome:')
+print(nome)
+idade = input('Digite sua idade:')
+print(idade)
+print('Meu nome é :' + nome + ' e a idade: ' + idade)
+idade = int(idade) + 1
+print('Meu nome é :' + nome + ' e no ano que vem terei: ' + str(idade))
