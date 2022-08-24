1021f7c89f2f6f70bcef1253472d5fb99b29dccf
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2022-08-24 20:35:39 -0300

    Desafio - 24/08/2022

---
+++ desafio.py
@@ -0,0 +1,14 @@
+dados = []
+dados.append((1, 'Orlando', 'Fatec Araras'))
+dados.append((2, 'Nilton', 'Fatec Araras'))
+dados.append(10)
+dados.append((3, 'João', 'Fatec Araras'))
+dados.append(30.4)
+dados.append([1,2,3])
+dados.append([])
+
+'''
+Crie um programa que imprima somente os nomes dos professores.
+A Estrutura de dado com os campos que queremos possui a seguinte estrutura:
+(<numero>, <nome>, <unidade>)
+'''
