2164dc2526bec3ebd14d586764385f84188c4376
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2022-09-14 22:06:16 -0300

    Feriados v.0.2

--- django/feriados/core/tests.py
+++ django/feriados/core/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase
+from django.test import TestCase    
 
 
 class NatalTest(TestCase):
--- django/feriados/core/views.py
+++ django/feriados/core/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def natal(request):
-    return HttpResponse("Não é natal.")
+    return render(request, 'natal.html')
