b65686c4127f0f93503a95348952abbbe329e6ac
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2022-09-21 20:23:08 -0300

    Revisão - Aula1 - Django

--- django/feriados/core/views.py
+++ django/feriados/core/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 
 def natal(request):
     contexto = {'natal': True, 'carnaval': False}
+    # import ipdb ; ipdb.set_trace()
     return render(request, 'natal.html', contexto)
