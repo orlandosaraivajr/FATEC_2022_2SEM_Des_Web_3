11bd0fe4cd438033bbe80030d0ea1edecbb11213
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2022-09-21 22:09:21 -0300

    Django 2 - ORM

--- django/feriados/core/tests.py
+++ django/feriados/core/tests.py
@@ -13,3 +13,29 @@ class NatalTest(TestCase):
 
     def test_template_natal(self):
         self.assertTemplateUsed(self.resp, 'natal.html')
+
+
+from core.models import FeriadoModel
+from datetime import datetime
+
+class FeriadoModelTest(TestCase):
+    def setUp(self):
+        self.feriado = 'Natal'
+        self.mes = 12
+        self.dia = 25
+        self.cadastro = FeriadoModel(
+            nome=self.feriado,
+            dia=self.dia,
+            mes=self.mes,
+        )
+        self.cadastro.save()
+    
+    def test_created(self):
+        self.assertTrue(FeriadoModel.objects.exists())
+
+    def test_modificado_em(self):
+        self.assertIsInstance(self.cadastro.modificado_em, datetime)
+
+    def test_nome_feriado(self):
+        nome = self.cadastro.__dict__.get('nome', '')
+        self.assertEqual(nome, self.feriado)
